3b099f73f51dc619d5c3817780af80d155f316c3
Daniel Braimah <danbrasafe@gmail.com>
2022-07-26 03:36:18 -0400

    Improved CSS GRIDS

--- src/routes/Portfolio.js
+++ src/routes/Portfolio.js
@@ -9,24 +9,55 @@ function Portfolio() {
       <div className='portfolio-container'>
         <h1 className='port-header'>Portfolio</h1>
 
-        {/*
-        <div className='wrapper'>
-          <div className='header'>HEADER</div>
-          <div className='menu'>MENU</div>
-          <div className='content'>CONTENT</div>
-          <div className='footer'>FOOTER</div>
-        </div>
-  */}
-
         <div className='grid-container'>
-          <div className='header'> HEADER</div>
-          <div className='menu'>MENU</div>
-          <div className='content'>CONTENT</div>
+          <div className='header'>
+            <img
+              className='image-styles'
+              alt='school'
+              src='/images/Guelph_U.jpeg'
+            />
+            HEADER
+            <p className='test-text'>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt
+              commodi quos earum quod dolore, ducimus enim nihil, dolores
+              consequuntur nostrum nam doloremque corporis fugiat facilis
+              voluptates optio ullam accusamus eos? Lorem ipsum dolor sit, amet
+              consectetur adipisicing elit. Deserunt commodi quos earum quod
+              dolore, ducimus enim nihil, dolores consequuntur nostrum nam
+              doloremque corporis fugiat facilis voluptates optio ullam
+              accusamus eos? Lorem ipsum dolor sit, amet consectetur adipisicing
+              elit. Deserunt commodi quos earum quod dolore, ducimus enim nihil,
+              dolores consequuntur nostrum nam doloremque corporis fugiat
+              facilis voluptates optio ullam accusamus eos? elit. Deserunt
+              commodi quos earum quod dolore, ducimus enim nihil, dolores
+              consequuntur nostrum nam doloremque corporis fugiat facilis
+              voluptates optio ullam accusamus eos? elit. Deserunt commodi quos
+              earum quod dolore, ducimus enim nihil, dolores consequuntur
+              nostrum nam doloremque corporis fugiat facilis voluptates optio
+              ullam accusamus eos? elit. Deserunt commodi quos earum quod
+              dolore, ducimus enim nihil, dolores consequuntur nostrum nam
+              doloremque corporis fugiat facilis voluptates optio ullam
+              accusamus eos?
+            </p>
+          </div>
+          <div className='menu'>
+            <img className='kang-img' alt='school' src='/images/Kang.jpeg' />
+            MENU
+          </div>
+          <div className='content'>
+            <p className='test-text'>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt
+              commodi quos earum quod dolore, ducimus enim nihil, dolores
+              consequuntur nostrum nam doloremque corporis fugiat facilis
+              voluptates optio ullam accusamus eos?
+            </p>
+            CONTENT
+          </div>
           <div className='my-footer'>FOOTER</div>
         </div>
       </div>
     </>
-  );
+  )
 }
 
 export default Portfolio;
